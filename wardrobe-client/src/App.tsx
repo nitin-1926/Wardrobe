065893dee9281eacaab97251b85677176098cba2
Nitin Gupta <nitin@rocketium.com>
2024-02-20 22:14:07 +0530

    feat: setting up antd and sidebar menu UI

--- wardrobe-client/src/App.tsx
+++ wardrobe-client/src/App.tsx
@@ -1,35 +1,200 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import type { MenuProps, MenuTheme } from 'antd';
+import { ConfigProvider, Layout, Menu, theme } from 'antd';
+import styled from 'styled-components';
+import {
+	HomeOutlined,
+	SearchOutlined,
+	CompassOutlined,
+	HeartOutlined,
+	PlusCircleOutlined,
+	UserOutlined,
+	MenuOutlined,
+	SunOutlined,
+	MoonOutlined,
+	LogoutOutlined,
+	SettingOutlined,
+	InstagramOutlined,
+} from '@ant-design/icons';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Header, Content, Footer, Sider } = Layout;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const notToBeSelectedKeys = ['theme', 'settings', 'logout'];
 
-export default App
+const App: React.FC = () => {
+	const {
+		token: { colorBgContainer, borderRadiusLG },
+	} = theme.useToken();
+
+	const [themeMode, setThemeMode] = useState<MenuTheme>('light');
+	const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
+
+	const sideBarItems: MenuProps['items'] = [
+		{
+			key: 'home',
+			icon: React.createElement(HomeOutlined),
+			label: 'Home',
+		},
+		{
+			key: 'search',
+			icon: React.createElement(SearchOutlined),
+			label: 'Search',
+		},
+		{
+			key: 'explore',
+			icon: React.createElement(CompassOutlined),
+			label: 'Explore',
+		},
+		{
+			key: 'notifications',
+			icon: React.createElement(HeartOutlined),
+			label: 'Notifications',
+		},
+		{
+			key: 'addPost',
+			icon: React.createElement(PlusCircleOutlined),
+			label: 'Create',
+		},
+		{
+			key: 'profile',
+			icon: React.createElement(UserOutlined),
+			label: 'Profile',
+		},
+		{
+			key: 'userMenu',
+			icon: React.createElement(MenuOutlined),
+			label: 'Menu',
+			children: [
+				{
+					key: 'theme',
+					icon: themeMode === 'light' ? React.createElement(MoonOutlined) : React.createElement(SunOutlined),
+					label: 'Switch appearance',
+				},
+				{
+					key: 'settings',
+					icon: React.createElement(SettingOutlined),
+					label: 'Settings',
+				},
+				{
+					key: 'logout',
+					icon: React.createElement(LogoutOutlined),
+					label: 'Logout',
+				},
+			],
+		},
+	];
+
+	const handleMenuItemSelect: MenuProps['onClick'] = e => {
+		switch (e.key) {
+			case 'home':
+				console.log('home');
+				break;
+			case 'search':
+				console.log('search');
+				break;
+			case 'explore':
+				console.log('explore');
+				break;
+			case 'notifications':
+				console.log('notifications');
+				break;
+			case 'addPost':
+				console.log('addPost');
+				break;
+			case 'profile':
+				console.log('profile');
+				break;
+			case 'theme':
+				setThemeMode(prev => (prev === 'light' ? 'dark' : 'light'));
+				break;
+			case 'settings':
+				console.log('settings');
+				break;
+			case 'logout':
+				console.log('logout');
+				break;
+			default:
+				break;
+		}
+		// Adding selected keys
+		if (notToBeSelectedKeys.includes(e.key)) return;
+		setSelectedKeys([e.key]);
+	};
+
+	const just = (
+		<Layout hasSider>
+			<StyledSideBar collapsed collapsible={false} collapsedWidth={60} theme={themeMode}>
+				<StyledLogo />
+				<StyledMenu
+					selectedKeys={selectedKeys}
+					theme={themeMode}
+					mode="inline"
+					defaultSelectedKeys={['profile']}
+					items={sideBarItems}
+					onClick={handleMenuItemSelect}
+				/>
+			</StyledSideBar>
+			<Layout style={{ marginLeft: 200 }}>
+				{/* Hiding header as not needed for now */}
+				<Header style={{ padding: 0, background: colorBgContainer }} hidden />
+				<Content style={{ margin: '24px 16px 0', overflow: 'initial', height: '100vh' }}>
+					<div>HELLO WORLD</div>
+				</Content>
+				<Footer style={{ textAlign: 'center' }} hidden>
+					Wardrobe ©{new Date().getFullYear()} Created with ❤️ by Wardrobe Team
+				</Footer>
+			</Layout>
+		</Layout>
+	);
+
+	return (
+		<ConfigProvider
+			theme={{
+				// Algorithm
+				algorithm: themeMode === 'light' ? theme.defaultAlgorithm : theme.darkAlgorithm,
+				token: {
+					// Seed Token
+					colorPrimary: '#8a3ffc',
+					colorInfo: '#8a3ffc',
+				},
+			}}
+		>
+			{just}
+		</ConfigProvider>
+	);
+};
+
+const StyledSideBar = styled(Sider)`
+	overflow: hidden;
+	// overflow-y: auto;
+	// overflow-x: hidden;
+
+	// hide scrollbar
+	-ms-overflow-style: none;
+	scrollbar-width: none;
+	&::-webkit-scrollbar {
+		display: none;
+	}
+
+	height: 100vh;
+	position: fixed !important;
+	top: 0;
+	left: 0;
+	bottom: 0;
+`;
+
+const StyledMenu = styled(Menu)`
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+const StyledLogo = styled(InstagramOutlined)`
+	font-size: 24px;
+	color: white;
+	margin: 16px;
+`;
+
+export default App;
